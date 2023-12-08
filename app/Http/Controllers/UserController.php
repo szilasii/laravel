0232f0d5aec1a6a4300793a8168b53766e560aa7
szilasii <szilasii78@gmail.com>
2023-12-08 11:22:39 +0100

    add controler update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -34,5 +34,40 @@ class UserController extends Controller
                 "user" => $User
             ],201);
     }
+
+    public function show($id) {
+        $User = User::find($id);
+        if (empty($User)){
+            return response()->json([
+                "message" => "Nincs ilyen felhasználó"
+            ],404);
+        } 
+        return response()->json($User);
+    }
+
+    public function update(Request $request, int $id) : string {
+       if (User::where('id',$id)->exists()) {
+        try {   $User = User::find($id);
+                $User->name = is_null($request->name) ? $User->name : $request->name ;
+                $User->email = is_null($request->email) ? $User->email : $request->email;
+                $User->password = is_null($request->password) ? $User->password : bcrypt($request->password,[PASSWORD_BCRYPT]);
+                $User->accountNumber = is_null($request->accountNumber) ? $User->accountNumber : $request->accountNumber;
+                //$User->token = jwt token amit generálok
+                $User->save();
+               } catch (Exception $e ){
+                    return response()->json([
+                        "message" => "Hiba a mentéskor",
+                        "error" => $e],404);
+               }    
+       }
+
+       return response()->json([
+        "message" => "Nincs ilyen felhasználó"
+        ],404);
+
+
+
+
+    }
 }
 
